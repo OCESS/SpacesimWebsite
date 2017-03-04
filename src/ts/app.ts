2c549ffc8dc4596af4ba28bedee9a434c70afd9b
Ivan Fonseca <ivanfon@riseup.net>
2017-03-04 13:26:21 -0500

    Fixed bug with downloads page and InstantClick.

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,5 +1,17 @@
-$(function () {
+// Import InstantClick (sorta)
+declare var InstantClick: any;
+
+// Loads popovers on page
+const loadPopovers = () => {
   $('[data-toggle="popover"]').popover({
-      html: true
-    });
+    html: true
+  });
+}
+
+// Initial page load
+$(document).ready(loadPopovers);
+
+// Capture InstantClick page changes
+InstantClick.on('change', () => {
+  loadPopovers();
 });
